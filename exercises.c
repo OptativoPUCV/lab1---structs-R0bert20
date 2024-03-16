7d5f78fd30da3f67570549398c86f86952becd1e
Your Name <you@example.com>
2024-03-16 21:33:27 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,21 +39,24 @@ y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
-  int* newArr = NULL;
+  int *newArr = (int *)malloc(size * sizeof(int));
+  if (newArr == NULL) {
+      *newSize = 0;
+      return NULL; 
+  }
   *newSize = 0;
-  for (int i = 0; i < size; i++){
-    if (arr[i] % 2 == 0 ){
-      int* temp = (int*)realloc(newArr, (*newSize + 1) * sizeof(int));
-      if (temp == NULL) {
-        free(newArr);
-        exit(EXIT_FAILURE);
+  for (int i = 0; i < size; i++) {
+      if (arr[i] % 2 == 0) {
+          newArr[*newSize] = arr[i];
+          (*newSize)++;
       }
-      newArr = temp;
-      newArr[*newSize] = arr[i];
-      (*newSize)++;
-      }
-    }
-  return newArr;  
+  }
+  newArr = (int *)realloc(newArr, (*newSize) * sizeof(int));
+  if (newArr == NULL && *newSize > 0) {
+      free(newArr);
+      *newSize = 0;
+  }
+  return newArr;
   }
 
 /*
